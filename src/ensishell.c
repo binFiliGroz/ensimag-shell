80b601982ce6f781f70bce683bab18c388f6c5ed
Angelo FILIPPI <angelo.filippi@theaf.xyz>
2016-09-23 16:02:23 +0200

    ajout de commentaires

--- src/ensishell.c
+++ src/ensishell.c
@@ -36,26 +36,29 @@ pid_t launch_command (struct cmdline *l) {
         case -1:
                 perror("fork:");
                 break;
+	// si on se trouve dans le processus fils
         case 0:
         {
 		char **cmd = l->seq[0];
+		// fermeture des entrées/sorties standards si 
+		// processus en tâche de fond
 		if (l->bg) {
 			fclose(stdin);
 			fclose(stdout);
 			fclose(stderr);
 		}
+		// execution de la commande
                 execvp(cmd[0], cmd);
                 break;
         }
-
+	//si on se trouve dans le processus père
         default:
         {
+		// si le processus est au premier plan
+		// alors on attends la fin de l'éxecution de la commande
 		if (!(l->bg)) {
                 	int status;
                 	waitpid(pid, &status, 0);
-		}
-		else {
-			
 		}
                 break;
         }
@@ -171,6 +174,7 @@ int main() {
 
 		printf("\n");
 		*/
+
 		launch_command(l);
 	}
 
